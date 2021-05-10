c4df0a9afa9c58b234c66f1553d40463f421af9a
kimorion <kimorion@ya.ru>
2021-05-10 22:54:06 +0300

    add frontend

---
+++ agnis-frontend/src/app/auth/components/register/register.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+}
